afc54beaafe38b39f1274a5f54c4f13a17df4d6c
Rushi <189489rishi@gmail.com>
2022-02-24 21:06:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Find Maximum Integer
+
+using System;
+
+namespace Find_Max_Using_Generics
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine(" Generics");
+            FindMaximumInt findMax = new FindMaximumInt();
+            Console.WriteLine(findMax.MaxInt(30, 20, 10)); 
+            Console.WriteLine(findMax.MaxInt(30, 40, 10));  
+            Console.WriteLine(findMax.MaxInt(30, 20, 40));  
+        }
+    }
+}
